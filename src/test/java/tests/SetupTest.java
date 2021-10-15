c73519f4a096c4f96f9048457a59659f8adfa5ff
Elcio Augusto Dalosto <edalosto@gmail.com>
2021-10-15 20:10:12 -0300

    pequenas implementações

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -3,6 +3,7 @@ package tests;
 import Utils.Browser;
 import Utils.Utils;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import static org.junit.Assert.assertTrue;
 
@@ -10,7 +11,23 @@ public class SetupTest extends BaseTests{
 
     @Test
     public void testOpeningBrowserAndLoadingPage(){
-        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl()));
-        System.out.println("Abrimos o navegador e carregamos a URL");
+        boolean baseUrl = Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl());
+        assertTrue(baseUrl);
+//        System.out.println("Abrimos o navegador e carregamos a URL");
+    }
+
+    @Test
+    public void testLogin(){
+        Browser.getCurrentDriver().findElement(By.className("login")).click();
+//        System.out.println("click login");
+        boolean caminhoPaginaDeLogin = Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl().concat("index.php?controller=authentication&back=my-account"));
+        assertTrue(caminhoPaginaDeLogin);
+        Browser.getCurrentDriver().findElement(By.id("email")).sendKeys("fakemail@fakemail.com");
+        Browser.getCurrentDriver().findElement(By.id("passwd")).sendKeys("fakepass");
+        Browser.getCurrentDriver().findElement(By.id("SubmitLogin")).click();
+        boolean caminhoPaginaAutenticado = Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl().concat("index.php?controller=my-account"));
+        assertTrue(caminhoPaginaAutenticado);
+        boolean textoPageHeading = Browser.getCurrentDriver().findElement(By.className("page-heading")).getText().contains("MY ACCOUNT");
+        assertTrue(textoPageHeading);
     }
 }
